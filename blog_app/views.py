6e10d096a9c0ca7999b90edab98beb89283f34c3
Atharva Jibhakate <atharvajibhakate@gmail.com>
2021-02-11 00:20:30 +0530

    pagination not working

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -18,6 +19,18 @@ class PostListView(ListView):
     template_name='blog_app/home.html' # <app>/<model>_<viewtype>
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 4
+
+class UserPostListView(ListView):
+    model = Post
+    template_name='blog_app/user_posts.html' # <app>/<model>_<viewtype>
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 4
+
+    def get_query_set(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
